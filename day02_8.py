60421d0db92898bb825ba4b07f39871a993d1ec5
twdssqrtdd <ksm1025kk@naver.com>
2023-01-08 13:32:58 +0900

    python day2

---
+++ day02_8.py
@@ -0,0 +1,25 @@
+# 횟수를 입력받아서 그 수만큼 Hello 출력
+# ex) 5 입력시 1번째 Hello ~ 5번째 Hello 출력
+
+# count = int(input('반복할 횟수를 입력하시오. '))
+# i = 0
+# while i < count : 
+#     i += 1
+#     print(i,'번째 Hello')
+
+# 1 ~ 100 사이에서 7의 배수만 출력하는 프로그램(while 안에서 if 를 사용)
+# i = 1
+# while i < 100:
+#     if(i % 7) == 0:
+#         print(i)
+#     i += 1
+
+# 커피 1 잔에 300원, 금액을 입력하여 몇 잔의 커피와 잔돈이 얼마나 남는지 출력
+i = int(input('금액은 얼마인가요? '))
+while()
+int(print(i / 300))
+int(print(i % 300))
+
+
+        
+
