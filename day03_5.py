9a4d04c18a58ef38c044635b26b94651bd5f4620
twdssqrtdd <ksm1025kk@naver.com>
2023-01-14 13:10:22 +0900

    파이썬 3일차

---
+++ day03_5.py
@@ -0,0 +1,47 @@
+# 문자열 = 'hello world, my name is python'
+
+# for i in 문자열:
+#     print(i, end=' ')
+
+# i = 0
+# while i < len(문자열):
+#     print(문자열p[i], and=' ')
+#     i += 1 
+
+# 문자열에서 알파벳 o의 갯수를 구하시오
+
+# 문자열 = 'hello world, my name is python'
+# k = 'o'
+# count = 0
+# for i in 문자열:
+#     if(i == k):
+        
+
+
+
+# # 1월 ~ 12월을 출력하되 입력받은 월은 skip
+# k = int(input('입력하시오'))
+# for i in range(1, 13):
+#     if i == k:
+#         continue
+#     print(i, '월')
+# print()
+
+# # 1월 ~ 12월을 출력하되 입력받은 월부터 출력하지 않음
+# p = int(input('입력하시오'))
+# for j in range(1, 13):
+#     if j == p:
+#         break
+#     print(j, '월')
+# print()
+
+# subway1 = 10
+# subway2 = 12
+# subway3 = 15
+# 리스트 = [10 ,12 ,15]
+# for i in 리스트:
+#     print(i, '명')
+
+
+    
+
